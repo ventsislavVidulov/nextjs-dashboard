31f7647d134b957976175a38f5743556c2acf902
ventsislavVidulov <xcen3k@gmail.com>
2026-04-13 15:09:52 +0300

    add not-found page

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
@@ -10,7 +11,11 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
         await fetchInvoiceById(id),
         await fetchCustomers()
     ])
-    
+
+    if (!invoice) {
+        notFound();
+    }
+
     return (
         <main>
             <Breadcrumbs
